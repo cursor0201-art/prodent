854d94c52965804b531bd5d094739d9455b02c30
Futuree <uldashevshuhrat123@gmail.com>
2026-07-27 22:36:22 +0500

    fix(deployment): add .koyeb.app to ALLOWED_HOSTS

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -34,11 +34,12 @@ SECRET_KEY = env('SECRET_KEY', default='dummy-secret-key-for-build')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env('DEBUG', default=False)
 
-ALLOWED_HOSTS = [
+ALLOWED_HOSTS = env('ALLOWED_HOSTS', default=[
     'localhost',
     '127.0.0.1',
-    'prodent-hfae.onrender.com'
-]
+    'prodent-hfae.onrender.com',
+    '.koyeb.app',
+])
 
 
 # Application definition
